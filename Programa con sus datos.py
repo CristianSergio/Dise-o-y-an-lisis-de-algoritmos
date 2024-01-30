0eabc328308092e4d61a450fdb2b5ab182a4e0f0
CristianSergio <68801667+CristianSergio@users.noreply.github.com>
2024-01-30 10:58:26 -0600

    Add files via upload

---
+++ Programa con sus datos.py
@@ -0,0 +1,9 @@
+def main():
+    nombre = "Cristian Sergio Acuña Mora"  
+    numero_control = "XXXX"  
+
+    print("Nombre: ", nombre)
+    print("Número de control: ", numero_control)
+
+if __name__ == "__main__":
+    main()
